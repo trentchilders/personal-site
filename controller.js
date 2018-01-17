4f8d5dc63b551e63d3e1591f4a8a88d526890c64
Trent Childers <trent65@msn.com>
2018-01-16 21:02:14 -0600

    added video player

--- controller.js
+++ controller.js
@@ -1,14 +1,10 @@
 
 var init = function(){
-    console.log('here');
+    
+    
+
 }
 
 var sendAlert = function(){
     alert('Hello back');
-}
-
-var toggleDropdown = function(){
-    debugger
-    var hamburger = document.getElementById('hamburger');
-    hamburger.dropdown();
 }
